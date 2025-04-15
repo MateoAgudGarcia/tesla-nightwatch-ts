f9ca5e665603b472e844e9e5b50e67ca3deabb76
Mateo Agudelo Garcia <mateo.agudelo80@eia.edu.co>
2025-04-15 12:22:54 -0500

    feat: add environment properties file

--- globals.js
+++ globals.js
@@ -1,4 +1,7 @@
 import { NightwatchAllureReporter } from 'nightwatch-allure';
+import fs from 'fs';
+import os from 'os';
+import process from 'process';
 
 export const abortOnAssertionFailure = true;
 export const waitForConditionPollInterval = 500;
@@ -17,14 +20,51 @@ export function reporter(results, done) {
   reporter.write(results, done);
 }
 
+let chromeVersion;
+
 // const US_REGION = '.tds-country--us > a';
 
 export async function beforeEach(browser, done) {
+  const capabilities = await browser.driver.getCapabilities();
+  chromeVersion = capabilities.getBrowserVersion();
+
   await browser.window.setSize(1500, 1000);
   await browser.navigateTo('https://www.tesla.com/ ');
   // await browser.click(US_REGION);
   done();
 }
 export function afterEach(browser, done) {
+  done();
+}
+
+function createEnvironmentProperties() {
+  try {
+    const osInfo = `${os.type()} ${os.release()}`;
+    const osVersion = os.version();
+    const nodeVersion = process.versions.node;
+
+    const content = [
+      `Node.Version=${nodeVersion}`,
+      `Platform=${osInfo}`,
+      `Os.Version=${osVersion}`,
+      `Browser=${chromeVersion}`,
+    ].join('\n');
+
+    const allureResultsDir = 'allure-results';
+    if (!fs.existsSync(allureResultsDir)) {
+      fs.mkdirSync(allureResultsDir);
+    }
+
+    fs.writeFileSync(`${allureResultsDir}/environment.properties`, content);
+  } catch (error) {
+    throw new Error(
+      'File environment.properties had not been generated',
+      error,
+    );
+  }
+}
+
+export function after(done) {
+  createEnvironmentProperties();
   done();
 }
